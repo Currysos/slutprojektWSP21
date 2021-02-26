fb77f467eecffc9bb1b0eb2625a801aa8464c614
Currysos <nohj.gbg@gmail.com>
2021-02-26 11:21:39 +0100

    Jag vet inte riktigt allt som ändrats men jag har lagt till lite i Projektplaneringen och gjort ett sequence diagram

--- app/app.rb
+++ app/app.rb
@@ -1,1 +1,116 @@
-print "This is a app"
+# frozen_string_literal: true
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+
+enable :sessions
+get('/') do
+  slim(:register)
+end
+
+get('/showlogin') do
+  slim(:login)
+end
+
+post("/login") do 
+    username = params[:username]
+    password = params[:password]
+
+    db = SQLite3::Database.new("db/todo.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+
+    begin
+      pwdigest = result["pwdigest"]
+      id = result["id"]
+    rescue NoMethodError => exception
+      return "Användaren finns inte"
+    end
+
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+
+      redirect("/todos")
+    else
+      "Fel lösen"
+    end
+end
+
+get("/todos") do
+  id = session[:id].to_i
+
+  db = SQLite3::Database.new("db/todo.db")
+  db.results_as_hash = true
+
+  result = db.execute("SELECT * FROM todos WHERE user_id = ?", id)
+
+
+  slim(:"todos/index", locals:{todos:result})
+end
+
+post("/todos/new") do 
+  content = params[:content]
+  id = session[:id]
+
+  if(content == "")
+    redirect("/todos")
+  end
+
+  db = SQLite3::Database.new("db/todo.db")
+  db.execute("INSERT INTO todos (content,user_id) VALUES (?,?)", content, id)
+
+  redirect("/todos")
+end
+
+get('/todos/:id/edit') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM todos WHERE id = ?",id).first
+  slim(:"todos/edit", locals:{result:result})
+end
+
+post('/todos/:id/update') do
+  id = params[:id].to_i
+  content = params[:content]
+  db = SQLite3::Database.new("db/todo.db")
+
+  db.execute("UPDATE todos SET content=? WHERE id=?", content, id)
+
+  redirect('/todos')
+end
+
+get('/todos/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo.db")
+  
+  db.execute("DELETE FROM todos WHERE id = ?", id)
+
+  redirect('/todos')
+end
+
+post("/users/new") do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  if(username == "")
+    return "Du glömde användarnamnet"
+  end
+  if(password == "")
+    return "Du glömde att skriva in ett lösenord, vilket är rekommenderat"
+  end
+
+  if password == password_confirm
+    #Lägg till användare
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new("db/todo.db")
+    db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)", username, password_digest)
+    redirect("/")
+  else
+    return "Lösenorden matchade inte!"
+  end
+end
--- projektplan.md
+++ projektplan.md
@@ -1,8 +1,27 @@
-# Projektplan
+# NTI-Annex Projektplan
 
-## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra).
+## 1. Projektbeskrivning
+### Abstract
+Min app, NTI-Annex, kommer att vara ett chatforum där elever ska kunna chatta med varandra, likt google chat fast med ett lättare gränssnitt och darkmode. Man kommer att skapa ett konto för att sedan kunna lägga till vänner, chatta med dessa vänner och skapa grupper om man så vill. När man skapar sitt konto så behöver man ange vilken klass man går i. Detta eftersom alla ska veta vilka de är. Appen kan användas till att skapa klasschatter eller bara allmänt chatter för skolaktiviteter.
+### Design
+Jag kommer att fokusera på mobila enheter eftersom den största andelen som surfar på internet är från mobil. Det känns dessutom skönast att kommunicera via sms på mobila enheter. 
+
+Designen kommer att vara simpel med endast ett få antal funktioner för att uppfylla de fem gränssnittsdesign-principerna. Appen kommer att vara i darkmode, både för att det är skönast för ögonen att titta på men också för att ge en speciell känsla under användning. Jag kan inte riktigt beskriva den men du kommer märka när du använder appen ;).
+### Databas
+Databasen utgår ifrån users, eftersom users kommer att spara den viktigaste informationen. Det kommer sedan finnas tabeller med kurser inom skolan, gruppchatter och meddelanden. All info, exempelvis ett meddelande, kommer att tillges främmande nycklar för att sedan kunna användas i relationstabeller. Detta leder till ett smidigt sätt att skapa och hantera chatter, användare och grupper. 
+
+#### Exempel
+##### Sign_up:
+![Sign_up sequence](NTI-Annex%20Sequence%20Diagram%20sign_up.png)
+
 ## 2. Vyer (visa bildskisser på dina sidor).
+
 ## 3. Databas med ER-diagram (Bild på ER-diagram).
+#Er diagram
+Hur databasen kommer att sitta ihop:
+![ER-Diagram](ER%20Diagram%20NTI%20Annex.png)
+
+
 ## 4. Arkitektur (Beskriv filer och mappar - vad gör/innehåller de?).
 
 
